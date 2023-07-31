78d93070393bb942170783e66cdf21507dd813cb
podlunoy <greg.demidenkov@yandex.ru>
2023-07-31 18:14:26 +0300

    Fix app import

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 
-import { App } from './app/App.js'
+import { App } from '@app/App.js'
 import '@app/styles/reseter.scss'
 import { store } from '@app/store/rootStore.js'
 
